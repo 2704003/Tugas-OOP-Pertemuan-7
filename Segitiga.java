5357b7bb32c5a6320013e75ae0a62618f9264ada
2704003 <tiaraputrimaheswarichandra@gmail.com>
2022-11-12 01:29:39 +0700

    Program Bangun Datar

---
+++ Segitiga.java
@@ -0,0 +1,13 @@
+public class Segitiga extends BangunDatar {
+    int alas = 5;
+    int tinggi = 8;
+
+    @Override
+    public float luas() {
+        return alas * tinggi * 1/2;
+    }
+    @Override
+    public float keliling() {
+        return alas + alas + alas;
+    }
+}
