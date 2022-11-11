d9593ae476144299475ed13d52d461c108d16afd
2704003 <tiaraputrimaheswarichandra@gmail.com>
2022-11-12 01:26:29 +0700

    Program Bangun Datar

---
+++ Lingkaran.java
@@ -0,0 +1,13 @@
+public class Lingkaran extends BangunDatar {
+    int r = 6;
+
+
+    @Override
+    public float luas() {
+        return (float) (3.14 * r * r);
+    }
+    @Override
+    public float keliling() {
+        return (float) (3.14 * r * 1/4);
+    }
+}
