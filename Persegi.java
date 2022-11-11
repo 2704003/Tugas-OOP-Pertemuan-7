39d462755c4f3e1d689e79804c82b4f152fc2a15
2704003 <tiaraputrimaheswarichandra@gmail.com>
2022-11-12 01:28:31 +0700

    Program Bangun Datar

---
+++ Persegi.java
@@ -0,0 +1,12 @@
+public class Persegi extends BangunDatar {
+    int sisi = 5;
+
+    @Override
+    public float luas() {
+        return sisi * sisi;
+    }
+    @Override
+    public float keliling() {
+        return 4 * sisi;
+    }
+}
